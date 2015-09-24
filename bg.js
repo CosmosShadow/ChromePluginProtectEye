0469100cf54322ef36fdb335d07bb1e409ec13fd
lichen <lichenarthurml@gmail.com>
2015-09-24 16:06:39 +0800

    push

---
+++ bg.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+        $("body").css("background-color", "#FFFFED");
+        $("*").css("background-color", "#FFFFED");
+        $("*").css("color", "black");
+        // var elements = $("*");
+        // for (var i = 0; i < elements.length; i++) {
+        //         var element = elements[i];
+        //         var z_index = $(element).css("z-index");
+        //         if (IsNumber(z_index)){
+        //                 $(element).css("background-color", "#FFFFED");
+        //         }
+        // };
+});
